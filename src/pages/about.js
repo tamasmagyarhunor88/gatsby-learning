fa7c15b52d2f37c14a3cd70fa29134fe8673e473
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2022-01-12 10:31:03 +0000

    Add basic about

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About Hunor</title>
+            <h1>About Hunor</h1>
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby. Lorem ypsum dolor etc</p>
+        </main>
+    )
+}
+
+export default AboutPage
