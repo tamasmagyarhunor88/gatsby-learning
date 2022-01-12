fbc9d698ddf7782d88f0a329e12e60ec29c1f435
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2022-01-12 16:38:54 +0000

    Update AboutPage,IndexPage

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout";
 const AboutPage = () => {
     return (
         <Layout pageTitle="About Me">
-            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby. Lorem ypsum dolor etc</p>
+            <p>Hi there! I'm Hunor and I'm the proud creator of this site, which I built with Gatsby. Lorem ypsum dolor etc</p>
         </Layout>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import Layout from "../components/layout";
 
 const IndexPage = () => {
   return (
-      <Layout pageTitle="Home Page">
-          <p>I'm making this by following the Gatsby Tutorial.</p>
+      <Layout pageTitle="Hunors Gatsby Home Page">
+          <p>Hi, I am Hunor and I'm making this by following the Gatsby Tutorial.</p>
       </Layout>
   )
 }
