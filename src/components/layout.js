24d18466f3dfe11098c94d997296d6d7baf91432
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2022-01-12 20:28:48 +0000

    Add css and pull it in the Layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,25 @@
 import * as React from 'react'
 import { Link } from "gatsby"
+import {
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText
+} from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
     return (
-        <div>
+        <div className={container}>
             <title>{pageTitle}</title>
             <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}><Link className={navLinkText} to="/">Home</Link></li>
+                    <li className={navLinkItem}><Link className={navLinkText} to="/about">About</Link></li>
                 </ul>
             </nav>
             <main>
-                <h1>{pageTitle}</h1>
+                <h1 className={heading}>{pageTitle}</h1>
                 {children}
             </main>
         </div>
