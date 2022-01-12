72dd8f6f8717e4f2f84aaa032fba78d33b0f4fbf
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2022-01-12 16:10:01 +0000

    Add Link component to index and about pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
+import { Link } from "gatsby";
 
 const AboutPage = () => {
     return (
         <main>
             <title>About Hunor</title>
             <h1>About Hunor</h1>
+            <Link to="/">Back to Home</Link>
             <p>Hi there! I'm the proud creator of this site, which I built with Gatsby. Lorem ypsum dolor etc</p>
         </main>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
       <main>
-        <title>GatsbyJS Hunor ltd</title>
-        <h1>Welcome to Hunors Gatsby site!</h1>
-        <p>I'm making this by following the Gatsby Tutorial.</p>
+          <title>GatsbyJS Hunor ltd</title>
+          <h1>Welcome to Hunors Gatsby site!</h1>
+          <Link to="/about">About</Link>
+          <p>I'm making this by following the Gatsby Tutorial.</p>
       </main>
   )
 }
