52346a8162a32c582c0176282fc8bb4c8608fbc0
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2022-01-13 19:13:09 +0000

    Update Gatsby config, add StaticImage component to IndexPage component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
     siteMetadata: {
+        title: "Hunor GatsbyJS learning",
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
-    ]
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+    ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
-import Layout from "../components/layout";
+import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
       <Layout pageTitle="Hunors Gatsby Home Page">
           <p>Hi, I am Hunor and I'm making this by following the Gatsby Tutorial.</p>
+          <StaticImage src="https://bringatrailer.com/wp-content/uploads/2020/05/2020_porsche_gt4_158946692401fb71c976a628c37220200429_182312.jpg" alt="Porsche 718 GT4"/>
       </Layout>
   )
 }
