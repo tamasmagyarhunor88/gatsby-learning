bbec0650741a88a8cf26479d3558837a5bdbc42a
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2022-01-14 23:14:11 +0000

    Add blog files, update BlogPage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
     siteMetadata: {
         title: "Hunor GatsbyJS lrng",
-        siteUrl: `https://www.hunorlearning.xyz`,
+        siteUrl: `https://www.learnhunor.xyz`,
     },
     plugins: [
         "gatsby-plugin-image",
         "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: `blog`,
+                path: `${__dirname}/blog`,
+            }
+        },
     ],
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,30 @@
 import * as React from 'react'
 import Layout from "../components/layout"
+import { graphql } from "gatsby";
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My non existing Blog posts">
-            <p>My non existing cool blog posts will go here</p>
+            <ul>
+                {
+                    data.allFile.nodes.map(node =>(
+                        <li key={node.name}>{node.name}</li>
+                    ))
+                }
+            </ul>
         </Layout>
     )
 }
+
+export const query = graphql`
+    query {
+        allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+            nodes {
+                name
+                sourceInstanceName
+            }
+        }
+    }
+`
 
 export default BlogPage
