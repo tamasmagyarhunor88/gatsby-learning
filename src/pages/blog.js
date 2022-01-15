99429e0ef986b82710bfc706e0cc601d49ae183f
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2022-01-15 21:54:17 +0000

    Use allMdx GraphQL query to query mdx nodes and use MDXRenderer to render mdx content in React components

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,22 @@
 import * as React from 'react'
 import Layout from "../components/layout"
 import { graphql } from "gatsby";
+import {MDXRenderer} from "gatsby-plugin-mdx"
 
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My non existing Blog posts">
             <ul>
                 {
-                    data.allFile.nodes.map(node =>(
-                        <li key={node.name}>{node.name}</li>
+                    data.allMdx.nodes.map((node) => (
+                        <article key={node.id}>
+                            <h2>{node.frontmatter.title}</h2>
+                            <p>Posted: {node.frontmatter.date}</p>
+                            <p>Last updadted: {node.parent.modifiedTime}</p>
+                            <MDXRenderer>
+                                {node.body}
+                            </MDXRenderer>
+                        </article>
                     ))
                 }
             </ul>
@@ -18,10 +26,19 @@ const BlogPage = ({ data }) => {
 
 export const query = graphql`
     query {
-        allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
             nodes {
-                name
-                sourceInstanceName
+                frontmatter {
+                    date(formatString: "DD MMMM, YYYY")
+                    title
+                }
+                id
+                body
+                parent {
+                    ... on File {
+                        modifiedTime(formatString: "DD MMMM, YYYY")
+                    }
+                }
             }
         }
     }
