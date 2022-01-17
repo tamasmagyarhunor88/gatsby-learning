b16f42a72060f48f1ee6c3b056f923ad1080c4b9
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2022-01-17 21:06:22 +0000

    Add mdx slug, placeholder content

---
+++ src/pages/{mdx.slug}.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const BlogPost = () => {
+    return (
+        <Layout pageTitle="Placeholder title">
+            <p>Content will be here</p>
+        </Layout>
+    )
+}
+
+export default BlogPost
