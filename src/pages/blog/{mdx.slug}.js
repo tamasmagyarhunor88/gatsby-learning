9baf15099519e20b9e3d18405914f3e134d87c11
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2022-01-18 19:42:54 +0000

    Update photo path, move blog.mdx-s back to their folder

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,15 +1,27 @@
 import * as React from 'react'
 import Layout from '../../components/layout'
-import {MDXRenderer} from "gatsby-plugin-mdx"
-import {graphql} from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { graphql } from "gatsby"
 
 const BlogPost = ({ data }) => {
+    const image = getImage(data.mdx.frontmatter.hero_image)
+
     return (
         <Layout pageTitle={data.mdx.frontmatter.title}>
-            <p>{data.mdx.frontmatter.date}</p>
+            <p>Posted: {data.mdx.frontmatter.date}</p>
             <MDXRenderer>
                 {data.mdx.body}
             </MDXRenderer>
+            <GatsbyImage
+                alt={data.mdx.frontmatter.hero_image_alt}
+                image={image}/>
+            <p>
+                Photo Credit:{" "}
+                <a href={data.mdx.frontmatter.hero_image_credit_link}>
+                    {data.mdx.frontmatter.hero_image_credit_text}
+                </a>
+            </p>
         </Layout>
     )
 }
@@ -17,11 +29,19 @@ const BlogPost = ({ data }) => {
 export const query = graphql`
     query ($id: String) {
         mdx(id: {eq: $id}) {
+            body
             frontmatter {
                 title
-                date(formatString: "DD MMMM, YYYY")
+                date(formatString: "MMMM D, YYYY")
+                hero_image_alt
+                hero_image_credit_link
+                hero_image_credit_text
+                hero_image {
+                    childImageSharp {
+                        gatsbyImageData
+                    }
+                }
             }
-            body
         }
     }
 `
