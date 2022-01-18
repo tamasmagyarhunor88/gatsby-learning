6743426acd7126bd24837337f6ef617817c0fee4
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2022-01-18 19:01:44 +0000

    Include dependency in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
                 path: `${__dirname}/blog`,
             }
         },
-        "gatsby-plugin-mdx"
+        "gatsby-plugin-mdx",
+        "gatsby-transformer-sharp",
     ],
 }
